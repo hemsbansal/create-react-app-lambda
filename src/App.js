c75ac8445c020d27c97672f61f02d3df35b17c90
Hemant Kumar <hemsbansal@gmail.com>
2022-09-17 13:50:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class LambdaDemo extends Component {
       <p>
         <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
         <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
+        <br />{"Hemsbansal"}
         <span>{msg}</span>
       </p>
     )
